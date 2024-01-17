858965fa760ff1ac1fd903bbb6bbd74a5a20b60d
Diana <1005201@mborijnland.nl>
2024-01-17 13:44:23 +0100

    update index and header

--- header.php
+++ header.php
@@ -8,16 +8,24 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Hardstyle Shop</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <link rel="stylesheet" type="text/css" href="stylen.css">
+    <link rel="stylesheet" type="text/css" href="styles.css">
 </head>
 <header>
-    <nav>
+    <nav class="logo">
         <ul>
-            <li><a href="index.php"><img src="asset/mborijnland logo (1).png" style="width:15%"></a></li>
+            <li><img src="image/logo.jpg" style="width:575px"></a></li>
+            <li><img src="image/Stephanie%20Magic%20Photo%20crop.png" style="width:101px"></a></li>
+            <li><a href="login.php">Inlog</a></li>
+        </ul>
+    </nav>
+    <nav class="navigation">
+        <ul>
             <li><a href="index.php">Home</a></li>
-
-            <li><a href="roomview.php">kamers</a></li>
-            <li><a href="login.php">Login</a></li>
+            <li><a href="#">Webshop</a></li>
+            <li><a href="#">Gallery</a></li>
+            <li><a href="#">Contact</a></li>
+        <!--alleen te zien voor eigenaar en medewerker(s-->
+            <li><a href="#" style="color: #666565">Dashboard</a></li>
         </ul>
     </nav>
 </header>
