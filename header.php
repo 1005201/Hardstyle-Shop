1e0573b6a9a2c1385bf7e23f4b9194c70ac0b148
Diana <106817549+1005201@users.noreply.github.com>
2024-01-17 10:29:07 +0100

    Add files via upload

---
+++ header.php
@@ -0,0 +1,24 @@
+<?php
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hardstyle Shop</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="stylen.css">
+</head>
+<header>
+    <nav>
+        <ul>
+            <li><a href="index.php"><img src="asset/mborijnland logo (1).png" style="width:15%"></a></li>
+            <li><a href="index.php">Home</a></li>
+
+            <li><a href="roomview.php">kamers</a></li>
+            <li><a href="login.php">Login</a></li>
+        </ul>
+    </nav>
+</header>
+</html>
